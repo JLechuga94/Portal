186fc4cd03df4f75d8c0e3aa74f794dfa6c7aa7c
Julian Lechuga <julian.lechuga305@gmail.com>
2018-06-25 16:14:13 +0900

    Initial Commit

---
+++ AR-Portal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR-Portal
+//
+//  Created by Julian Lechuga Lopez on 25/6/18.
+//  Copyright © 2018 Julian Lechuga Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
